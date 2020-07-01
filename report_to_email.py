d90f8a45ff291e3fe872b790d887420620d409e9
Toma1997 <toma.joksimovic@gmail.com>
2020-07-02 01:05:01 +0200

    Refactored py

--- report_to_email.py
+++ report_to_email.py
@@ -9,69 +9,84 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-def crtanjePodataka(temW2U,tempThingSpeak):
+def getEmailSender(brojMejla):
+    _, msg = imap.fetch(str(int(brojMejla)), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            msg = emailDecoder.message_from_bytes(response[1])
+            from_who = msg.get("From").split()[-1].replace("<","").replace(">","")
+            return from_who
+
+def fetchtempWeather2Umbrella():
+    respWeather2Umbrella = requests.get("https://www.weather2umbrella.com/vremenska-prognoza-beograd-srbija-sr/trenutno")
+    weather_per_hours = BeautifulSoup(respWeather2Umbrella.text, "html.parser").find('div', attrs={'class':'weather_per_hours'})
+    hourly_temp = weather_per_hours.find_all('div', attrs={'class':'col-xs-2 col-sm-1 hourly_temp'})
+    tempList = []
+    print(hourly_temp)
+    for temp in hourly_temp:
+        tempList.append(int(temp.p.text.replace("°","")))
+    hours = int(time.strftime("%H"))
+    start = (hours - 6) if (hours - 6) > 0 else 1
+    tempWeather2Umbrella = []
+    for i in range(start - 1, hours):
+        tempWeather2Umbrella.append(tempList[i])
+
+    # Add here to look for last 7 hours with previous day.
+    if len(tempWeather2Umbrella) == 0:
+        tempWeather2Umbrella = [0] * 7
+    return tempWeather2Umbrella 
+
+def fetchTempThingSpeak():
+    respThingSpeak= requests.get("https://api.thingspeak.com/channels/1091866/feeds.json?api_key=547RK4B5HP5VKXHV&results=7")
+    values = respThingSpeak.json()['feeds']
+    tempThingSpeak = []
+    for val in values:
+        tempThingSpeak.append(int(float(val['field1'])))
+    return tempThingSpeak
+
+def plotTemperatures(tempWeather2Umbrella, tempThingSpeak):
     f = plt.figure()
-    plt.plot(range(1,8),tempW2U,color="green",label="Weather2Umbrella")
-    plt.plot(range(1,8),tempThingSpeak,color="orange",label="ThingSpeak")
+    plt.plot(range(1,8), tempWeather2Umbrella, color="green", label="Weather2Umbrella")
+    plt.plot(range(1,8), tempThingSpeak, color="orange", label="ThingSpeak")
     plt.ylabel("Temperature")
     plt.xlabel("Samples")
     plt.legend()
     f.savefig("temperature.png")
 
-def dohvatanjeTempW2U():
-    urlW2U = "https://www.weather2umbrella.com/vremenska-prognoza-beograd-srbija-sr/trenutno"
-    respW2U = requests.get(urlW2U)
-    weather_per_hours = BeautifulSoup(respW2U.text, "html.parser").find('div', attrs={'class':'weather_per_hours'})
-    hourly_temp = weather_per_hours.find_all('div', attrs={'class':'col-xs-2 col-sm-1 hourly_temp'})
-    tempList = list()
-    for temp in hourly_temp:
-        tempList.append(int(temp.p.text.replace("°","")))
-    sati = int(time.strftime("%H"))
-    tempW2U = list()
-    pocetak = (sati-6) if(sati-6)>0 else 1
-    for i in range(pocetak-1,sati):
-        tempW2U.append(tempList[i])
-    return tempW2U 
-
-def dohvatanjeTempThingSpeak():
-    respThingSpeak= requests.get("https://api.thingspeak.com/channels/1091866/feeds.json?api_key=547RK4B5HP5VKXHV&results=7")
-    merenja = respThingSpeak.json()['feeds']
-    tempThingSpeak = list()
-    for m in merenja:
-        tempThingSpeak.append(int(float(m['field1'])))
-    return tempThingSpeak
-
-def dohvatanjeOstalihPodatakaThingSpeak(slanjeIzvestaja):
+def getCoolerHeaterAvg(lastReportNum):
     respThingSpeak= requests.get("https://api.thingspeak.com/channels/1091866/feeds.json?api_key=547RK4B5HP5VKXHV")
-    merenja = respThingSpeak.json()['feeds']
-    motor, grejac = list(), list()
-    for m in merenja:
-        motor.append(int(float(m['field2'])))
-        grejac.append(int(m['field3']))
-    novoPodaciMotor, novoPodaciGrejac = list(), list()
-    for broj in range(slanjeIzvestaja, len(motor)):
-        novoPodaciMotor.append(round(motor[broj] / 255, 1) * 100) # Scale to percentage.
-        novoPodaciGrejac.append(grejac[broj])
-    slanjeIzvestaja = len(grejac)
-    brojPaljenjaGrejaca = sum(novoPodaciGrejac)
-    prosecanRadVentilatora = round(sum(novoPodaciMotor)/len(novoPodaciMotor), 2) if len(novoPodaciMotor) > 0 else 0
-    return prosecanRadVentilatora, brojPaljenjaGrejaca, slanjeIzvestaja
+    values = respThingSpeak.json()['feeds']
+    coolerAllValues = []
+    heaterAllValues = []
+    for val in values:
+        coolerAllValues.append(int(float(val['field2'])))
+        heaterAllValues.append(int(val['field3']))
+    afterLastReportCoolerValues = []
+    afterLastReportHeaterValues = []
+    for indx in range(lastReportNum, len(coolerAllValues)):
+        afterLastReportCoolerValues.append(round(coolerAllValues[indx] / 255, 1) * 100) # Scale to percentage.
+        afterLastReportHeaterValues.append(heaterAllValues[indx])
+    lastReportNum = len(heaterAllValues)
+    countHeatOn = sum(afterLastReportHeaterValues)
+    avgCoolerPower = round(sum(afterLastReportCoolerValues)/len(afterLastReportCoolerValues), 2) if len(afterLastReportCoolerValues) > 0 else 0
+    return avgCoolerPower, countHeatOn, lastReportNum
 
-def merenjaOtvaranjaGaraze(imap):
-    _, newGarageOpenings = imap.search(None,'SUBJECT "Garage" UNSEEN')
+def getNewGarageOpeningsCount(imap):
+    response, newGarageOpenings = imap.search(None,'SUBJECT "Garage" UNSEEN')
+    print("IMAP response searching for mail subject 'Garage': " + response)
     countNewGarageOpenings = len(newGarageOpenings[0].split())
     for i in newGarageOpenings[0].split():
-        imap.store(i,'+FLAGS','\\SEEN')
+        imap.store(i, '+FLAGS', '\\SEEN')
     return countNewGarageOpenings
 
-def kreiranjeIzvestaja(email, password, to, prosecanRadVentilatora, brojPaljenjaGrejaca, countNewGarageOpenings):
+def createReport(email, password, to, avgCoolerPower, countHeatOn, countNewGarageOpenings):
     report = MIMEMultipart()
     report["Subject"] = "RESPORT RESPONSE"
     report["From"] = email
     report["To"] = to
     mimeTextsValues = {time.strftime("%d.%m.%Y %H:%M"): "<b>Report at {}</b><br>", 
-                    prosecanRadVentilatora: "Average Cooler power since the last report: {0:.2f} %<br>",
-                    brojPaljenjaGrejaca: "Number of heater activation since the last report: {0:d}<br>",
+                    avgCoolerPower: "Average Cooler power since the last report: {0:.2f} %<br>",
+                    countHeatOn: "Number of heater activation since the last report: {0:d}<br>",
                     countNewGarageOpenings: "Number of garage openings since the last report: {0:d}<br>" }
     for key in mimeTextsValues.keys():
         report.attach(MIMEText(mimeTextsValues[key].format(key), 'html'))
@@ -80,36 +95,37 @@ def kreiranjeIzvestaja(email, password, to, prosecanRadVentilatora, brojPaljenja
         imgPlot = MIMEImage(filePlot.read())
         imgPlot.add_header('Content-ID', '<image1>')
     report.attach(imgPlot)
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, to, report.as_string())
-    server.quit()
+    smtpServer = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpServer.starttls()
+    smtpServer.login(email, password)
+    smtpServer.sendmail(email, to, report.as_string())
+    smtpServer.quit()
 
-def odKogaJeMejl(brojMejla):
-    _, msg = imap.fetch(str(int(brojMejla)), "(RFC822)")
-    for response in msg:
-        if isinstance(response, tuple):
-            msg = emailDecoder.message_from_bytes(response[1])
-            from_who = msg.get("From").split()[-1].replace("<","").replace(">","")
-            return from_who
+def processDataAndCreateReport(request_mail_num, lastReportNum, imap, email, password):
+    to = getEmailSender(request_mail_num)
+    print("Fetching and processing temperatures from Thingspeak and Weather2Umbrella...")
+    tempWeather2Umbrella = fetchtempWeather2Umbrella()
+    tempThingSpeak = fetchTempThingSpeak()
+    plotTemperatures(tempWeather2Umbrella, tempThingSpeak)
+    print("Fetching and processing cooler and heater average values after last report...")
+    avgCoolerPower, countHeatOn, lastReportNum = getCoolerHeaterAvg(lastReportNum)
+    print("Getting garage openings emails since last report...")
+    countNewGarageOpenings = getNewGarageOpeningsCount(imap)
+    print("New report is creating...")
+    createReport(email, password, to, avgCoolerPower, countHeatOn, countNewGarageOpenings)
+    return lastReportNum
 
 if __name__ == "__main__":
-    sendReport = 0
+    lastReportNum = 0
+    email = "toma.demo97@gmail.com"
+    password = "Tomademo97+"
     while True:
-        email = "toma.demo97@gmail.com"
-        password = "Tomademo97+"
         imap = imaplib.IMAP4_SSL('imap.gmail.com')
         imap.login(email, password)
         print("Logged in!")
         imap.select("INBOX")
-        _, izvestaj = imap.search(None, 'SUBJECT "IZVESTAJ" UNSEEN')
-        for i in izvestaj[0].split():
-            to = odKogaJeMejl(i)
-            tempW2U = dohvatanjeTempW2U()
-            tempThingSpeak = dohvatanjeTempThingSpeak()
-            crtanjePodataka(tempW2U, tempThingSpeak)
-            prosecanRadVentilatora, brojPaljenjaGrejaca, sendReport = dohvatanjeOstalihPodatakaThingSpeak(sendReport)
-            countNewGarageOpenings = merenjaOtvaranjaGaraze(imap)
-            print("Report is creating...")
-            kreiranjeIzvestaja(email, password, to, prosecanRadVentilatora, brojPaljenjaGrejaca, countNewGarageOpenings)
+        response, report = imap.search(None, 'SUBJECT "IZVESTAJ" UNSEEN')
+        print("IMAP response searching for mail subject 'IZVESTAJ': " + response)
+        for request_mail_num in report[0].split():
+            lastReportNum = processDataAndCreateReport(request_mail_num, lastReportNum, imap, email, password)
+            
